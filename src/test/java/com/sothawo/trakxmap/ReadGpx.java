0e886d2f564153243e2a3986a3d179b643213cd3
P.J.Meisch <pj.meisch@sothawo.com>
2015-01-14 17:37:13 +0100

    modified test program

--- src/test/java/com/sothawo/trakxmap/ReadGpx.java
+++ src/test/java/com/sothawo/trakxmap/ReadGpx.java
@@ -19,16 +19,18 @@ import java.nio.file.Paths;
  */
 public class ReadGpx {
     public static void main(String[] args) {
-        try {
-            Unmarshaller unmarshaller =
-                    JAXBContext.newInstance(GpxType.class.getPackage().getName()).createUnmarshaller();
-//            Path path = Paths.get("/Users/peter/tmp", "Test.gpx");
-//            Path path = Paths.get("/Users/peter/tmp", "Fahrrad.gpx");
-            Path path = Paths.get("/Users/peter/tmp", "Sellaronda-Marmolada.gpx");
-            GpxType gpxType = ((JAXBElement<GpxType>) unmarshaller.unmarshal(path.toFile())).getValue();
-            System.out.println(gpxType);
-        } catch (JAXBException e) {
-            e.printStackTrace();
+        if (args.length > 0) {
+
+            try {
+                Unmarshaller unmarshaller =
+                        JAXBContext.newInstance(GpxType.class.getPackage().getName()).createUnmarshaller();
+                Path path = Paths.get(args[0]);
+                @SuppressWarnings("unchecked")
+                GpxType gpxType = ((JAXBElement<GpxType>) unmarshaller.unmarshal(path.toFile())).getValue();
+                System.out.println(gpxType);
+            } catch (JAXBException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
