bfdcbe023d4420c5016dca16ef879e1196f619a6
P.J.Meisch <pj.meisch@sothawo.com>
2015-02-02 22:33:31 +0100

    preparation for db integration

---
+++ src/main/java/com/sothawo/trakxmap/util/PathTools.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2015 Jaroso GmbH
+ *
+ * http://www.jaroso.de
+ */
+package com.sothawo.trakxmap.util;
+
+/**
+ * TODO: description.
+ *
+ * @author P.J.Meisch (pj.meisch@jaroso.de)
+ */
+public class PathTools {
+}
